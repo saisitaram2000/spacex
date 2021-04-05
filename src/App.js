e8e236d92914e63eece3bafb34773f4e41dd0786
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2021-04-05 21:10:09 +0530

    created react app and verified api is working or not

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import SpacexDetails from './components/SpacexDetails';
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+        <SpacexDetails/>
+    </div>
+  );
+}
+
+export default App;
