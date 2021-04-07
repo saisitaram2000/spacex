a6f2213914ec59003ad0e27cd2b371e82b15ae76
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2021-04-07 18:03:46 +0530

    removed commeted css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="spacex-header">
-                <p className="spacex-header-start">SPACEX MISSIONS</p>
+                SPACEX MISSIONS
             </div>
         )
     }
